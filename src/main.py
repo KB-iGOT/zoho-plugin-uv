02aa0e3055f7c2b56808776124a5526a037d7b6e
jayaprakashnarayanaswamy <jayaprakash.n@tarento.com>
2025-06-24 14:19:37 +0530

    First commit

---
+++ src/main.py
@@ -0,0 +1,73 @@
+import os
+from typing import Optional
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Header
+from dotenv import load_dotenv
+
+from src import utils
+
+load_dotenv()
+
+app = FastAPI(
+    title="Karmayogi Bharat ZOHO Ticket Support API",
+    description="API for the Karmayogi Bharat ZOHO ticket support service",
+    version="1.0.0",
+    docs_url=None
+)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["chrome-extension://*", "https://desk.zoho.in"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    """Root endpoint to verify server status."""
+    return {"message": "This is Karmayogi Bharat ZOHO ticket support service REST integration !!"}
+
+
+@app.get("/ticket/details/{ticket_id}")
+async def get_ticket_user_details(
+        ticket_id: str,
+        x_zoho_cookies: Optional[str] = Header(None, alias="X-Zoho-Cookies")
+):
+    print("Ticket ID:", ticket_id)
+    print("Zoho Cookies:", x_zoho_cookies)  # This will now contain the cookies
+
+    # # Use the cookies to make authenticated requests to Zoho APIs
+    # access_token = utils.get_access_token()
+    # print("Access Token: ", access_token)
+
+    # Pass the cookies to your Zoho API calls
+    ticket_details = utils.get_ticket_details(
+        ticket_id,
+        os.getenv("ZOHO_ORG_ID"),
+        access_token=None,
+        cookie=x_zoho_cookies  # Use the cookies here
+    )
+
+    if not ticket_details:
+        return {"error": "Ticket details not found."}
+
+    print("Ticket Details: ", ticket_details)
+    user_email = ticket_details.get("email")
+
+    if not user_email:
+        return {"error": "User email not found in ticket details."}
+
+    user_details = utils.get_user_details_from_igot(user_email)
+
+    # return user details if user details are found else throw an error
+    if user_details:
+        return {
+            "user_details": user_details
+        }
+    else:
+        return {"error": "User details not found for the provided email."}
---
+++ src/utils.py
@@ -0,0 +1,94 @@
+
+import os
+import requests
+
+def get_access_token() -> str:
+    """
+    curl --location --request POST 'https://accounts.zoho.in/oauth/v2/token?refresh_token={refresh_token}&scope=Desk.tickets.ALL%2CDesk.contacts.READ%2CDesk.search.READ&grant_type=refresh_token' \
+--header 'Cookie: _zcsr_tmp=2aaf399b-64ef-442e-bc5d-0990b7d50296; iamcsr=2aaf399b-64ef-442e-bc5d-0990b7d50296; zalb_6e73717622=cc36c6f8a6790832246efd66c032e512'
+    """
+
+    url = os.getenv("ZOHO_API_BASE_URL", "https://accounts.zoho.in")+os.getenv("ZOHO_ACCESS_TOKEN_API", "/oauth/v2/token")
+    payload = {
+        'refresh_token': os.getenv("ZOHO_REFRESH_TOKEN"),
+        'client_id': os.getenv("ZOHO_CLIENT_ID"),
+        'client_secret': os.getenv("ZOHO_CLIENT_SECRET"),
+        'scope': 'Desk.tickets.ALL,Desk.contacts.READ,Desk.search.READ',
+        'grant_type': 'refresh_token'
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    response = requests.post(url, data=payload, headers=headers)
+    print("get_access_token:: response:: ", response.text)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('access_token', '')
+    else:
+        raise Exception(f"Failed to get refresh token: {response.status_code} - {response.text}")
+
+
+def get_ticket_details(ticket_id: str, orgId: str, access_token: str, cookie: str) -> dict:
+    f"""
+    Fetch ticket details using the provided ticket ID and access token.
+    curl --location 'https://desk.zoho.in/api/v1/tickets/120349000058009045' \
+    --header 'orgId: 60023043070' \
+    --header 'Authorization: Zoho-oauthtoken access_token'
+    """
+    url = f"https://desk.zoho.in/api/v1/tickets/{ticket_id}"
+    headers = {
+        'orgId': orgId,
+    }
+
+    if access_token:
+        headers['Authorization'] = f'Zoho-oauthtoken {access_token}'
+    if cookie:
+        headers['Cookie'] = cookie
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Failed to fetch ticket details: {response.status_code} - {response.text}")
+
+
+def get_user_details_from_igot(email: str) -> dict:
+    """
+    Fetch user details from iGOT using the provided email.
+
+    curl --location 'https://portal.igotkarmayogi.gov.in/api/private/user/v1/search' \
+    --header 'Content-Type: application/json' \
+    --header 'Authorization: Bearer {api_key}' \
+    --data-raw '{
+        "request": {
+            "filters": {
+                "email": "anshu.aggarwal28@karmayogi.in"
+            },
+            "limit": 1,
+            "sort_by": {"createdDate": "desc"}
+        }
+    }'
+    """
+    url = os.getenv("IGOT_API_BASE_URL", "https://portal.igotkarmayogi.gov.in/api")+os.getenv("IGOT_SEARCH_USER_API", "/private/user/v1/search")
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {os.getenv("IGOT_API_KEY")}',
+    }
+    payload = {
+        "request": {
+            "filters": {
+                "email": email
+            },
+            "limit": 1,
+            "sort_by": {"createdDate": "desc"}
+        }
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        if data.get('result', {}).get('response').get('content'):
+            return data['result']['response']['content'][0]
+        else:
+            return {}
+    else:
+        raise Exception(f"Failed to fetch user details: {response.status_code} - {response.text}")
